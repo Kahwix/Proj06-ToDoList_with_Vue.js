8a39cc73f1ef4621696047d83eb4a8fc9d2ea277
52999103 <kahwaji.anthony@gmail.com>
2019-10-07 14:28:13 +0200

    Link TodoList.vue with App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <h1>Hello World !!!</h1>
+    <TodoList />
   </div>
 </template>
 
 <script>
-
+import TodoList from '@/components/TodoList.vue';
 
 export default {
   name: "app",
